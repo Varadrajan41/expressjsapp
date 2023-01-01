c1891227d63fed59f4975133a9f7c711e066aabb
Varadrajan <varadrajan123@gmail.com>
2023-01-02 00:21:21 +0530

    expressjs

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express= require("express")
 const app= express()
 
+const path=require("path")
+
 app.get("/",(req,res)=>{
-    res.send("Hi This is server")
+    res.sendFile(path.join(__dirname,"./public/index.htm"))
 })
 
 app.listen(3000,()=>{
-    console.log('listening the port 8000')
+    console.log('listening the port 8000');
 })
